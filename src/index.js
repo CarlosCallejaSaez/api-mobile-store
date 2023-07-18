d140b34be0d28bb6217ecc32eb994c242c7e3d91
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-07-18 17:47:39 +0200

    mock+404+500

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ const express = require('express');
 const app = express();
 const { connectionDB } = require('./config/db.js');
 
+app.use(express.json());
 
+app.use('/api', (req,res) => {res.send('Welcome to Mobile API');});
+
+
+
+app.use('*', (req, res, next) => {
+  res.status(404).json({ data: 'Error 404: Page not found' });
+});
+
+
+app.use((error, req, res, next) => {
+  res.status(500).json({ data: 'Internal Server Error' });
+});
 
 connectionDB();
 
