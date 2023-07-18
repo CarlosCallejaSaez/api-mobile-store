49976db750c33375df658b03bdd3d6897b7e3194
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-07-18 17:43:12 +0200

    server at port required

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strict', false)
+mongoose.set('strictPopulate', false)
+mongoose.set('strictQuery', false)
+
+const connectionDB = async () => {
+    try {
+      await mongoose.connect('mongodb://127.0.0.1:27017/') // localhost ---> 127.0.0.1 para que logre conectar
+      console.log('Conectado a la Base de Datos')
+    } catch (error) {
+      console.error(`No ha sido posible conectarse a la base de datos, error: ${error}`)
+    }
+  }
+  
+  module.exports = {connectionDB}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const { connectionDB } = require('./config/db.js');
+
+
+
+connectionDB();
+
+const PORT = 4001;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
